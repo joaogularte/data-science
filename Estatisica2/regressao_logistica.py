524c053a0d6cd43b2dc6e3023c8c421e19919aa1
joao gularte <jov96@hotmail.com>
2019-04-11 18:39:55 -0300

    Adicionado grafico

--- Estatisica2/regressao_logistica.py
+++ Estatisica2/regressao_logistica.py
@@ -4,15 +4,35 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
 
 base = pd.read_csv("../Dados/Eleicao.csv", sep=";")
-#plt.scatter(base.DESPESAS, base.SITUACAO)
 
 print("Informações estatisticas sobre a base: ")
 print(base.describe())
+print("")
 
+correlacao = np.corrcoef(base.DESPESAS, base.SITUACAO)
 print("Correlação entre DESPESAS e SITUACAO: ")
-np.corrcoef(base.DESPESAS, base.SITUACAO)
+print(correlacao)
 
 x = base.iloc[:, 2].values
 y = base.iloc[:, 1].values
 x = x[:, np.newaxis]
-print(x)
+
+modelo = LogisticRegression()
+modelo.fit(x, y)
+
+print("Valor de inclinação: ")
+print(modelo.coef_)
+print("Valor de intercepção: ")
+print(modelo.intercept_)
+
+X_teste = np.linspace(10, 3000, 100)
+
+def model(x):
+    return 1 / (1 + np.exp(-x))
+
+r = model(X_teste * modelo.coef_ + modelo.intercept_).ravel()
+
+
+plt.scatter(x, y)
+plt.plot(X_teste, r, color = "red")
+plt.show()
