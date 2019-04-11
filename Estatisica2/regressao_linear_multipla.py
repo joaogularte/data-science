55821ad2ca7d73c91e41b3e42eeb960e62651ac0
Joao Vitor Gularte <joaogularte@UNISC.BR>
2019-04-11 13:34:17 -0300

    Concluido exemplo de regressao linear multipla

--- Estatisica2/regressao_linear_multipla.py
+++ Estatisica2/regressao_linear_multipla.py
@@ -13,19 +13,40 @@ print("Correlacao entre x e y: ")
 print(correlacao)
 
 x = x.reshape(-1, 1)
+
 modelo = LinearRegression()
 modelo.fit(x, y)
-print('Intercepção: ')
+print('Valor de Intercepção: ')
 print(modelo.intercept_)
 
-print("Inclinação: ")
+print("Coeficiente de Inclinação: ")
 print(modelo.coef_)
 
 print("Coeficiente de determinação: ")
 print(modelo.score(x, y))
 
-#print("Coeficiente de determinação ajustado: ")
-
 previsoes = modelo.predict(x)
-print(previsoes)
-print("teste")
+
+print("Coeficiente de determinação ajustado: ")
+modelo_ajustado = sm.ols(formula = 'mpg ~ disp', data = base)
+modelo_treinado = modelo_ajustado.fit()
+print(modelo_treinado.summary())
+
+plt.scatter(x, y)
+plt.plot(x, previsoes, color = "red")
+plt.show()
+
+x1 = base.iloc[:, 1:4].values
+modelo.fit(x1, y)
+
+print("Coeficiente de determinação: ")
+print(modelo.score(x1, y))
+
+print("Coeficiente de determinação ajustado: ")
+modelo_ajustado2 = sm.ols(formula = 'mpg ~ cyl + disp + hp', data = base)
+modelo_treinado2 = modelo_ajustado2.fit()
+print(modelo_treinado2.summary())
+
+novo = np.array([4, 100, 200])
+novo = novo.reshape(1, -1)
+print(modelo.predict(novo))
